6a16a1261f20655cf74ff12a139734542cd39656
Oscar <dorian@kundigo.pro>
2023-09-25 10:55:42 +0200

    wip

--- semaphore/initial_setup.sh
+++ semaphore/initial_setup.sh
@@ -15,11 +15,11 @@ set -e
 
 ### SSH SETUP ###
 # Correct permissions since they are too open by default
-[[ -v BOTYGLOT_USE_CUSTOM_SSH_KEYS ]] && echo "======> chmod 0600 ~/.ssh/botyglot-*"
-[[ -v BOTYGLOT_USE_CUSTOM_SSH_KEYS ]] && chmod 0600 ~/.ssh/botyglot-*
+[[ -v BOTYGLOT_USE_CUSTOM_SSH_KEYS ]] && echo "======> chmod 0600 ~/.ssh/id_botyglot_*"
+[[ -v BOTYGLOT_USE_CUSTOM_SSH_KEYS ]] && chmod 0600 ~/.ssh/id_botyglot_*
 # Add the key to the ssh agent:
-[[ -v BOTYGLOT_USE_CUSTOM_SSH_KEYS ]] && echo "======> ssh-add ~/.ssh/botyglot-*"
-[[ -v BOTYGLOT_USE_CUSTOM_SSH_KEYS ]] && ssh-add ~/.ssh/botyglot-*
+[[ -v BOTYGLOT_USE_CUSTOM_SSH_KEYS ]] && echo "======> ssh-add ~/.ssh/id_botyglot_*"
+[[ -v BOTYGLOT_USE_CUSTOM_SSH_KEYS ]] && ssh-add ~/.ssh/id_botyglot_*
 
 ### INITIAL DEPENDECIES SETUP ###
 echo "======> sem-version ruby $NODE_VERSION -f"
