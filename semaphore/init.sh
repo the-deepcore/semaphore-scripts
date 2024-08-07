c50aba671d37cd3d636947683d9c824874d3cd97
Nicolas GONZALEZ <nicolas@thedeepcore.com>
2024-08-07 15:52:45 +0200

    Update init script: Setup postgresql, redis

---
+++ semaphore/init.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+sem-version node $NODE_VERSION -f
+sem-version ruby $RUBY_VERSION -f
+
+gem install bundler -v '2.3.7' --no-document
+bundle config set path 'vendor/bundle'
+rbenv rehash
+
+checkout --use-cache
+sem-service start postgres 15
+sem-service start redis 7
+
+exit 0
