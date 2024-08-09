8f3a7e70e0e96b240dc894ce6045eaa379af52f7
Nicolas GONZALEZ <nicolas@thedeepcore.com>
2024-08-09 15:42:12 +0200

    Update scripts

--- semaphore/init.sh
+++ semaphore/init.sh
@@ -4,7 +4,9 @@ set -e
 sem-version node 16.20.2
 sem-version ruby 3.2.0
 
-gem install bundler -v '2.3.7'
+gem install bundler -v '2.3.7' --no-document
+
+gem update --system 3.5.17
 
 checkout --use-cache
 
